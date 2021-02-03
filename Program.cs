f165b7edf2ef5f66cfeb2f55baeb56b9eb806953
Макс <yashanty@gmail.com>
2021-02-03 15:52:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GeometryTasks
+{
+    public class Vector
+    {
+        public double X;
+        public double Y;
+        //public void GetCoord(double x,double y)
+        //{
+        //    X = x;
+        //    Y = y;
+        //}
+    }
+    public class Geometry
+    {
+        public static double GetLength(Vector vector)
+        {
+            var vectorLength = Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+            return vectorLength;
+        }
+
+        public static Vector Add(Vector x, Vector y)
+        {
+            var dV = new Vector();
+            dV.X = x.X+y.X;
+            dV.Y = x.Y+y.Y;
+            return dV;
+        }
+    }
+}
+namespace vector1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GeometryTasks.Geometry.GetLength());
+            Console.WriteLine(GeometryTasks.Geometry.Add(10, 20));
+
+        }
+    }
+}
