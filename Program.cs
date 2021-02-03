153d60e7e85d3dc00780782fad813b204becb1d3
Макс <yashanty@gmail.com>
2021-02-03 16:03:05 +0300

    Revert "почистил ненужное"
    This reverts commit e0dcc52e1134252ed993cfd2ca1976ddd0bb028a.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace GeometryTasks
     {
         public double X;
         public double Y;
-        
+        //public void GetCoord(double x,double y)
+        //{
+        //    X = x;
+        //    Y = y;
+        //}
     }
     public class Geometry
     {
